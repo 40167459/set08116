160a2452bb8c1b12845c13dac004f2b3492c74d0
NAPIER-MAIL\40167459 <40167459@live.napier.ac.uk>
2017-02-27 14:19:50 +0000

    CW, Monday 27/02/2017.

--- labs/coursework/src/main.cpp
+++ labs/coursework/src/main.cpp
@@ -5,63 +5,155 @@ using namespace std;
 using namespace graphics_framework;
 using namespace glm;
 
-geometry geom;
+map<string, mesh> meshes;
 effect eff;
-target_camera cam;
+free_camera cam;
+texture tex;
 
 bool load_content() {
-  // Create triangle data
-  vector<vec3> positions{vec3(0.0f, 1.0f, 0.0f), vec3(-1.0f, -1.0f, 0.0f), vec3(1.0f, -1.0f, 0.0f)
+	// Create plane mesh
+	meshes["plane"] = mesh(geometry_builder::create_plane());
 
-  };
-  // Colours
-  vector<vec4> colours{vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f)};
-  // Add to the geometry
-  geom.add_buffer(positions, BUFFER_INDEXES::POSITION_BUFFER);
-  geom.add_buffer(colours, BUFFER_INDEXES::COLOUR_BUFFER);
+	// box
+	meshes["box"] = mesh(geometry_builder::create_box());
 
-  // Load in shaders
-  eff.add_shader("shaders/basic.vert", GL_VERTEX_SHADER);
-  eff.add_shader("shaders/basic.frag", GL_FRAGMENT_SHADER);
-  // Build effect
-  eff.build();
+	// Tetrahedron
+	meshes["tetrahedron"] = mesh(geometry_builder::create_tetrahedron());
+	// Pyramid
+	meshes["pyramid"] = mesh(geometry_builder::create_pyramid());
+	// Disk
+	meshes["disk"] = mesh(geometry_builder::create_disk());
+	// Cylinder
+	meshes["cylinder"] = mesh(geometry_builder::create_cylinder());
+	// Sphere
+	meshes["sphere"] = mesh(geometry_builder::create_sphere());
+	// Torus
+	meshes["torus"] = mesh(geometry_builder::create_torus());
 
-  // Set camera properties
-  cam.set_position(vec3(0.0f, 0.0f, 10.0f));
-  cam.set_target(vec3(0.0f, 0.0f, 0.0f));
-  cam.set_projection(quarter_pi<float>(), renderer::get_screen_aspect(), 0.1f, 1000.0f);
-  return true;
+	// Set the transforms for your meshes here
+	// 5x scale, move(-10.0f, 2.5f, -30.0f)
+	meshes["box"].get_transform().scale = vec3(5.0f, 5.0f, 5.0f);
+	meshes["box"].get_transform().translate(vec3(-10.0f, 2.5f, -30.0f));
+
+	// 4x scale, move(-30.0f, 10.0f, -10.0f)
+    meshes["tetrahedron"].get_transform().scale = vec3(4.0f, 4.0f, 4.0f);
+	meshes["tetrahedron"].get_transform().translate(vec3(-30.0f, 10.0f, -10.0f));
+	
+	// 5x scale, move(-10.0f, 7.5f, -30.0f)
+	meshes["pyramid"].get_transform().scale = vec3(5.0f, 5.0f, 5.0f);
+	meshes["pyramid"].get_transform().translate(vec3(-10.0f, 7.5f, -30.0f));
+
+	// scale(3.0f, 1.0f, 3.0f), move(-10.0f, 11.5f, -30.0f), 180 rotate X axis
+	meshes["disk"].get_transform().scale = vec3(3.0f, 1.0f, 3.0f);
+	meshes["disk"].get_transform().translate(vec3(-10.0f, 11.5f, -30.0f));
+
+	// 5x scale, move(-25.0f, 2.5f, -25.0f)
+	meshes["cylinder"].get_transform().scale = vec3(5.0f, 5.0f, 5.0f);
+	meshes["cylinder"].get_transform().translate(vec3(-25.0f, 2.5f, -25.0f));
+
+	// 2.5x scale, move(-25.0f, 10.0f, -25.0f)
+	meshes["sphere"].get_transform().scale = vec3(2.5f, 2.5f, 2.5f);
+	meshes["sphere"].get_transform().translate(vec3(-25.0f, 10.0f, -25.0f));
+
+	// 180 rotate X axis, move(-25.0f, 10.0f, -25.0f)
+	meshes["torus"].get_transform().translate(vec3(-25.0f, 10.0f, -25.0f));
+	meshes["torus"].get_transform().rotate(vec3(half_pi<float>(), 0.0f, 0.0f)); 
+
+	// Load texture
+	tex = texture("textures/grass.jpg");
+
+	// Load in shaders
+	eff.add_shader("shaders/simple_texture.vert", GL_VERTEX_SHADER);
+	eff.add_shader("shaders/simple_texture.frag", GL_FRAGMENT_SHADER);
+	// Build effect
+	eff.build();
+
+	//Load in Camera properties
+	cam.set_position(vec3(0.0f, 5.0f, 10.0f));
+	cam.set_target(vec3(0.0f, 0.0f, 0.0f));
+	cam.set_projection(quarter_pi<float>(), renderer::get_screen_aspect(), 0.1f, 1000.0f);
+	glfwSetInputMode(renderer::get_window(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	return true;
 }
-//add mesh plane
 
 bool update(float delta_time) {
-  // Update the camera
-  cam.update(delta_time);
-  return true;
+	// The ratio of pixels to rotation - remember the fov
+	static double ratio_width = quarter_pi<float>() / static_cast<float>(renderer::get_screen_width());
+	static double ratio_height =
+		(quarter_pi<float>() * renderer::get_screen_aspect()) / static_cast<float>(renderer::get_screen_height());
+	static double cursor_x = 0.0;
+	static double cursor_y = 0.0;
+	double current_x;
+	double current_y;
+	// Get the current cursor position
+	glfwGetCursorPos(renderer::get_window(), &current_x, &current_y);
+	// Calculate delta of cursor positions from last frame
+	double delta_x = current_x - cursor_x;
+	double delta_y = current_y - cursor_y;
+	// Multiply deltas by ratios - gets actual change in orientation
+	delta_x *= ratio_width;
+	delta_y *= ratio_height;
+	// Rotate cameras by delta
+	cam.rotate(delta_x, -delta_y);
+	// Use keyboard to move the camera - WASD
+	vec3 translation(0.0f, 0.0f, 0.0f);
+	if (glfwGetKey(renderer::get_window(), 'W')) {
+		translation.z += 5.0f * delta_time;
+	}
+	if (glfwGetKey(renderer::get_window(), 'S')) {
+		translation.z -= 5.0f * delta_time;
+	}
+	if (glfwGetKey(renderer::get_window(), 'A')) {
+		translation.x -= 5.0f * delta_time;
+	}
+	if (glfwGetKey(renderer::get_window(), 'D')) {
+		translation.x += 5.0f * delta_time;
+	}
+
+	// Move camera
+	cam.move(translation);
+	// Update the camera
+	cam.update(delta_time);
+	// Update cursor pos
+	cursor_x = current_x;
+	cursor_y = current_y;
+	return true;
 }
 
 bool render() {
-  // Bind effect
-  renderer::bind(eff);
-  // Create MVP matrix
-  mat4 M(1.0f);
-  auto V = cam.get_view();
-  auto P = cam.get_projection();
-  auto MVP = P * V * M;
-  // Set MVP matrix uniform
-  glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
-  // Render geometry
-  renderer::render(geom);
-  return true;
+	// Render meshes
+	for (auto &e : meshes) {
+		auto m = e.second;
+		// Bind effect
+		renderer::bind(eff);
+		// Create MVP matrix
+		auto M = m.get_transform().get_transform_matrix();
+		auto V = cam.get_view();
+		auto P = cam.get_projection();
+		auto MVP = P * V * M;
+		// Set MVP matrix uniform
+		glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
+
+		// *********************************
+		// Bind texture to renderer
+
+		// Set the texture value for the shader here
+
+		// *********************************
+		// Render mesh
+		renderer::render(m);
+	}
+
+	return true;
 }
 
 void main() {
-  // Create application
-  app application("Graphics Coursework");
-  // Set load content, update and render methods
-  application.set_load_content(load_content);
-  application.set_update(update);
-  application.set_render(render);
-  // Run application
-  application.run();
+	// Create application
+	app application("35_Geometry_Builder");
+	// Set load content, update and render methods
+	application.set_load_content(load_content);
+	application.set_update(update);
+	application.set_render(render);
+	// Run application
+	application.run();
 }
